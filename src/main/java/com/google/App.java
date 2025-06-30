3be889c55defdd0e5946a032ea8b25c181c12539
Karthik-Bharadwaj-A-R <55571778+Karthik-Bharadwaj-A-R@users.noreply.github.com>
2025-06-30 21:21:19 +0530

    Update App.java

--- src/main/java/com/google/App.java
+++ src/main/java/com/google/App.java
@@ -5,6 +5,6 @@ package com.google;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!, How are you");
+        System.out.println("Hello World!, Good");
     }
 }
