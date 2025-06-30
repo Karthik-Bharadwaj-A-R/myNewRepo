5c5e7affe3073e0c287a6fb45101d09638479f5f
Karthik-Bharadwaj-A-R <55571778+Karthik-Bharadwaj-A-R@users.noreply.github.com>
2025-06-30 21:26:34 +0530

    Update App.java

--- src/main/java/com/google/App.java
+++ src/main/java/com/google/App.java
@@ -5,6 +5,9 @@ package com.google;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!, Good");
+        int a=4;
+        int b=6;
+        int c = a+b;
+        System.out.println("The Sum is: "+c);
     }
 }
