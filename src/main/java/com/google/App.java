7e9f0b8032fcc3c4547497256ebe60f7ffe9349f
Karthik-Bharadwaj-A-R <55571778+Karthik-Bharadwaj-A-R@users.noreply.github.com>
2025-07-02 19:39:18 +0530

    Update App.java

--- src/main/java/com/google/App.java
+++ src/main/java/com/google/App.java
@@ -7,7 +7,7 @@ public class App {
     public static void main(String[] args) {
         int a=4;
         int b=6;
-        int c = a+b;
-        System.out.println("The Sum is: "+c);
+        int c = a-b;
+        System.out.println("The diff is: "+c);
     }
 }
